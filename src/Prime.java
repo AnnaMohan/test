56693ea564c8104b0692a5998174e5c1319386c5
Anna Mohan <anna.mohan@nagarro.com>
2021-03-09 18:16:09 +0530

    Inital commit

---
+++ src/Prime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter limit");
+		Scanner s= new Scanner(System.in);
+		int n=s.nextInt();
+		int num=2;
+		while(num<n) {
+			while(num<num-1) {
+				if(num%)
+			}
+			num++;
+		}
+	}
+
+}
